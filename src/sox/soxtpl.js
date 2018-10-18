f1a624e062d0913b59d8a191a94b2b484e17c817
六非 <43898279+sixno@users.noreply.github.com>
2018-10-18 09:29:11 +0800

    优化soxtpl

--- src/sox/soxtpl.js
+++ src/sox/soxtpl.js
@@ -9,9 +9,7 @@
 (function() {
 	"use strict";
 
-	function tpl(tpl){
-		var tpl = tpl || '';
-
+	function tpl(){
 		var config = {
 			tag_open: '{{',
 			tag_shut: '}}'
@@ -23,24 +21,23 @@
 			},
 			query: function(type, _, __){
 				var types = ['#([\\s\\S])+?','([^{#}])*?'][type || 0];
-				return this.exp((_||'') + config.tag_open + types + config.tag_shut + (__||''));
+				return this.exp((_ || '')+config.tag_open+types+config.tag_shut+(__ || ''));
 			},
 			escape: function(html){
-				return String(html||'').replace(/&(?!#?[a-zA-Z0-9]+;)/g, '&amp;')
-									   .replace(/</g, '&lt;')
-									   .replace(/>/g, '&gt;')
-									   .replace(/'/g, '&#39;')
-									   .replace(/"/g, '&quot;');
+				return String(html || '').replace(/&(?!#?[a-zA-Z0-9]+;)/g, '&amp;')
+										 .replace(/</g, '&lt;')
+										 .replace(/>/g, '&gt;')
+										 .replace(/'/g, '&#39;')
+										 .replace(/"/g, '&quot;');
 			},
 			error: function(e,log){
 				var error = 'soxtpl Error：';
-				typeof(console) == 'object' && console.error(error + e + '\n'+ (log || ''));
+				typeof(console) == 'object' && console.error(error+e+'\n'+(log || ''));
 				return error + e;
 			}
 		};
 
-		this.parse = function(tpl, data){
-			var that = this;
+		this.create = function(tpl){
 			var js_s = tool.exp('^'+config.tag_open+'#', '');
 			var js_e = tool.exp(config.tag_shut+'$', '');
 
@@ -50,7 +47,7 @@
 					 .replace(/\\/g, '\\\\').replace(/(?="|')/g, '\\')
 					 .replace(tool.query(), function(str){
 					 	str = str.replace(js_s, '').replace(js_e, '');
-					 	return '";' + str.replace(/\\/g, '') + ';view+="';
+					 	return '";'+str.replace(/\\/g, '')+';view+="';
 					 })
 					 .replace(tool.query(1), function(str){
 					 	var start = '"+(';
@@ -67,26 +64,22 @@
 					 			start = '"+_escape_(';
 					 		}
 
-					 		return start + str.replace(/\\/g, '') + ')+"';
+					 		return start+str.replace(/\\/g, '') + ')+"';
 					 });
 
-			tpl = '"use strict";var view = "' + tpl + '";return view;';
+			tpl = '"use strict";var view = "'+tpl+'";return view;';
 
 			try{
-				that.cache = tpl = new Function('d, _escape_', tpl);
-				return tpl(data, tool.escape);
-			} catch(e){
-				delete that.cache;
+				return new Function('d, _escape_', tpl);
+			}catch(e){
 				return tool.error(e, tpl);
 			}
 		};
 
-		this.render = function(data, callback){
-			var that = this;
-
+		this.render = function(tpl, data, callback){
 			if(!data) data = {};
 
-			tpl = that.cache ? that.cache(data, tool.escape) : that.parse(tpl, data);
+			tpl = tpl(data, tool.escape);
 
 			if(!callback) return tpl;
 
@@ -102,17 +95,17 @@
 			}
 		}
 		
-		this.version = '1.0';
+		this.version = '1.0.0';
 
 		return this;
 	}
 
 	if(typeof(soxui) != 'undefined')
 	{
-		soxui.tpl = tpl;
+		soxui.tpl = new tpl();
 	}
 	else
 	{
-		window.soxtpl = tpl;
+		window.soxtpl = new tpl();
 	}
 })();
